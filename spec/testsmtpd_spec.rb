7fbea2d82954cbe8714722b5924ea3f8bdb95566
Moritz Breit <mail@mobr.de>
2010-12-14 19:52:48 +0100

    Add first rspec test for testing TestSmtpd

--- spec/testsmtpd_spec.rb
+++ spec/testsmtpd_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Testsmtpd" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe TestSmtpd do
+  it "should start a server on port 1025" do
+    s = TestSmtpd.new {}
+    s.start
+    GServer.in_service?(1025).should be_true
+    s.stopped?.should be_false
+    s.stop
+    s.join
+    s.stopped?.should be_true
+    GServer.in_service?(1025).should be_false
   end
 end
+
